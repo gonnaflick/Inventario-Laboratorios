92b79f938e3dc404bcadd03beb8fadb015503480
gonnaflick <gonnaflick@hotmail.com>
2023-06-23 23:46:09 -0600

    New stepper design

--- src/app/pages/content/request/request.component.ts
+++ src/app/pages/content/request/request.component.ts
@@ -1,7 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { FormService } from 'src/app/pages/services/form.service';
-import { MatStepper } from '@angular/material/stepper';
+import { Component } from '@angular/core';
+
+interface Step {
+  label: string;
+  icon: string;
+  completed: boolean;
+  originalIcon: string;
+  animationClass: string;
+}
 
 @Component({
   selector: 'app-request',
@@ -9,19 +14,74 @@ import { MatStepper } from '@angular/material/stepper';
   styleUrls: ['./request.component.css'],
 })
 export class RequestComponent {
-  public firstStepForm: FormGroup;
-  public isFormValid: boolean = false;
+  steps: Step[] = [
+    {
+      label: 'Datos del solicitante',
+      icon: 'person',
+      completed: false,
+      originalIcon: 'person',
+      animationClass: '',
+    },
+    {
+      label: 'Escaneo de QR',
+      icon: 'qr_code_scanner',
+      completed: false,
+      originalIcon: 'qr_code_scanner',
+      animationClass: '',
+    },
+    {
+      label: 'Seleccion de equipo',
+      icon: 'shelves',
+      completed: false,
+      originalIcon: 'shelves',
+      animationClass: '',
+    },
+    {
+      label: 'Resumen y confirmacion',
+      icon: 'content_paste_search',
+      completed: false,
+      originalIcon: 'content_paste_search',
+      animationClass: '',
+    },
+  ];
 
-  @ViewChild('stepper') stepper?: MatStepper;
+  selectedIndex: number = 0;
+  isFormValid: boolean = false;
+  prevSelectedIndex: number = 0;
 
-  constructor(private formService: FormService) {
-    this.firstStepForm = this.formService.firstStepForm;
+  setStep(index: number) {
+    if (index === 0 || this.steps[index - 1].completed) {
+      this.prevSelectedIndex = this.selectedIndex;
+      this.selectedIndex = index;
+      this.updateStepIcons();
+    }
   }
 
   handleFormValid(valid: boolean) {
     this.isFormValid = valid;
     if (valid) {
-      this.stepper!.next();
+      this.steps[this.selectedIndex].completed = true;
+      this.prevSelectedIndex = this.selectedIndex;
+      this.selectedIndex++;
+      this.updateStepIcons();
     }
+  }
+
+  updateStepIcons() {
+    this.steps.forEach((step, index) => {
+      if (index < this.prevSelectedIndex && step.completed) {
+        step.icon = 'arrow_forward';
+        step.animationClass = 'icon-modify-animation';
+      } else if (index === this.selectedIndex && !step.completed) {
+        step.icon = step.originalIcon;
+        step.animationClass = 'icon-select-animation';
+      } else if (step.completed) {
+        step.icon = 'done';
+        step.animationClass = 'icon-complete-animation';
+      } else {
+        step.icon = step.originalIcon;
+        step.animationClass = '';
+      }
+    });
   }
 }
